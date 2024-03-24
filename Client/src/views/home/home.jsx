dadaf0aea2b27f03ae68e805ebf64f26f6e9b190
José Alberto <fuenmayorcoa@gmail.com>
2024-03-24 00:33:27 -0300

    Maqutado del Home listo, componentes Clients y Users listos

--- Client/src/views/home/home.jsx
+++ Client/src/views/home/home.jsx
@@ -3,17 +3,27 @@ import { useState } from "react";
 import SideBar from "../../components/sideBar/sideBar";
 import Clients from "../../components/clients/clients";
 import Users from "../../components/users/users";
+import "./home.css";
 
 function Home() {
-  const [selectedSection, setSelectedSection] = useState('clients');
+  const [selectedSection, setSelectedSection] = useState("clients");
 
   return (
     <div className="home-container">
       <SideBar onSelectedSection={setSelectedSection} />
-      {selectedSection === 'clients' && <Clients />}
-      {selectedSection === 'users' && <Users />}
+      <div className="main-content">
+        <header className="user-header">
+          <h2 className="user-name">Nombre de Usuario</h2>
+          <button className="logout-button">Logout</button>
+        </header>
+        <div className="content">
+          {selectedSection === "clients" && <Clients />}
+          {selectedSection === "users" && <Users />}
+        </div>
+      </div>
     </div>
   );
 }
 
 export default Home;
+
